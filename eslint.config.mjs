ca537997cccfbf9112157a60cb1bd7ca72f7cf82
Jean-Frédéric Faust <4367222+jffaust@users.noreply.github.com>
2026-03-06 12:48:13 -0500

    configure eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+// eslint.config.mjs
+import tsparser from "@typescript-eslint/parser";
+import { defineConfig } from "eslint/config";
+import obsidianmd from "eslint-plugin-obsidianmd";
+import globals from "globals";
+
+export default defineConfig([
+	{
+		ignores: [
+			"main.js",
+			"esbuild.config.mjs",
+			"version-bump.mjs",
+			"node_modules/**",
+		],
+	},
+	...obsidianmd.configs.recommended,
+	{
+		files: ["**/*.ts"],
+		languageOptions: {
+			parser: tsparser,
+			parserOptions: { project: "./tsconfig.json" },
+			globals: {
+				...globals.browser,
+				...globals.node,
+			},
+		},
+
+		// You can add your own configuration to override or add rules
+		rules: {
+			// example: turn off a rule from the recommended set
+			// "obsidianmd/sample-names": "off",
+			// example: add a rule not in the recommended set and set its severity
+			// "obsidianmd/prefer-file-manager-trash": "error",
+		},
+	},
+]);
